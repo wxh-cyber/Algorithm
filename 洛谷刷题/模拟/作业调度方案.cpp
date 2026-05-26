9752938ee67b351e83d644587dad431c9decb947
风雪心 <1097071510@qq.com>
2026-05-26 21:09:34 +0800

    feat(作业调度方案): 增加了新的刷题记录

---
+++ 洛谷刷题/模拟/作业调度方案.cpp
@@ -0,0 +1,68 @@
+/**
+ *  @link https://www.luogu.com.cn/problem/P1065
+ */
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int m,n;                                       //m台机器(同时也对应一个工件总共需要m步)，n个工件
+int list[501];                                 //list用于存储工件的加工顺序
+struct Information {
+    int id;                                    //在第id台机器上加工
+    int cost;                                  //花费cost时间
+} a[21][21];                                   //a[第几个工件][第几步]
+
+int mac[21][100001]={0};                       //mac[机器编号][时间]
+int step[21]={0};                              //每个工件加工到了第几步
+int las_time[21]={0};                          //每个工件最后加工的时间
+
+int main() {
+    scanf("%d%d",&m,&n);
+    // 输入加工顺序                       
+    for(int i=1;i<=m*n;i++){
+        scanf("%d",list+i);
+    }
+
+    //输入单个工件每个步骤应该在哪台机器上加工，以及花费的时间
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            scanf("%d",&a[i][j].id);
+        }
+    }
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            scanf("%d",&a[i][j].cost);
+        }
+    }
+
+    for(int i=1;i<=m*n;i++){
+        int now=list[i];                          //当前工件编号
+        step[now]++;                              //当前工件加工到了第几步
+        int id=a[now][step[now]].id;
+        int cost=a[now][step[now]].cost;
+
+        int s=0;
+        for(int j=las_time[now]+1;;j++) {
+            if(mac[id][j]==0) {
+                s++;
+            }else {
+                s=0;
+            }
+
+            if(s==cost) {
+                for(int k=j-cost+1;k<=j;k++){
+                    mac[id][k]=1;
+                }
+
+                if(j>ans) ans=j;
+                las_time[now]=j;
+                break;
+            }
+        }
+    }
+
+    printf("%d",ans);
+
+    return 0;
+}
