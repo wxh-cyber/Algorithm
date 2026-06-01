14f6a11f685c71dcec43cda98ec0e3882092581d
风雪心 <1097071510@qq.com>
2026-06-01 22:30:34 +0800

    docs(麦森数): 更新了题解注释

---
+++ 洛谷刷题/高精度/麦森数/解法二.cpp
@@ -0,0 +1,15 @@
+/**
+ *  @link https://www.luogu.com.cn/problem/P1045
+ */
+ #include <iostream>
+ #include <cmath>
+ using namespace std;
+
+ int p;
+ unsigned long long a[510]={0,1},t;
+ int main(){
+    cin>>p;
+    cout<<(int)(p*)
+
+    return 0;
+ }
