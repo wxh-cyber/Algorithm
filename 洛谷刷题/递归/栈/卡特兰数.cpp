88d9c19c9e0d5fa47b41c6c5f3c01ba3adc32fd4
风雪心 <1097071510@qq.com>
2026-07-14 08:57:49 +0800

    feat(栈、卡特兰数): 增加了刷题记录新的解法，同时增加了卡特兰数的推导

---
+++ 洛谷刷题/递归/栈/卡特兰数.cpp
@@ -0,0 +1,45 @@
+/**
+ *  @link https://www.luogu.com.cn/problem/P1044
+ */
+
+ /**
+  *  @note 注意：long long最大约为9.22*10^18，因此只要2n>=21，即n>=11，阶乘就溢出了。
+  *             因此本题采用边乘边除的形式，避免溢出。
+  */
+
+ #include <iostream>
+ typedef long long ll;
+ using namespace std;
+
+ int n;
+
+ /* 
+ // 采用递归的方法计算阶乘，会导致栈溢出
+ ll factorial(int n){
+    if(n==1) return 1;
+    return n*factorial(n-1);
+ }
+
+ ll combine(int n,int m){
+    return factorial(n)/(factorial(m)*factorial(n-m));
+ }
+ */
+
+
+ ll ktl(int n){
+    ll ans=1;
+
+    for(int i=1;i<=n;i++){
+        ans=ans*(n+i)/i;
+    }
+
+    return ans/(n+1);
+ }
+
+ int main(){
+    cin>>n;
+
+    cout<<ktl(n);
+
+    return 0;
+ }
